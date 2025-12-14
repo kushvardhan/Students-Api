03baaa49254fc0ba143b6e785cd1b2b2f84b9978
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-14 19:31:30 +0530

    load config, setup router, server , GRACEFULL SHUTDOWN

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/kushvardhan/Students-Api/pkg/config"
 )
 
 func main(){
-	r := mux.NewRouter();
+	// load config
 
-	log.Println("Starting Server at PORT:4444.");
-	log.Fatal(http.ListenAndServe(":4444",r))
+	cfg := config.MustLoad();
+
+	// setup router
+	router := http.NewServeMux();
+
+	router.HandleFunc("GET /",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"));
+
+	})
+
+	// server
+	server := http.Server{
+		Handler: router,
+		Addr: cfg.Address,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Address));
+
+	done := make(chan os.Signal,1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func(){
+		err := server.ListenAndServe();
+		if err != nil{
+			log.Fatal("Failed to start server");
+		}
+}()
+
+<- done
+
+slog.Info("Shutting down the server");
+
+ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+defer cancel()
+
+if err := server.Shutdown(ctx); err != nil{
+	slog.Error("Failed to shutdown server", slog.String("error", err.Error()));
 }
 
+slog.Info("sever shutdown successfully");
+
+}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 type HTTPServe struct{
-	Address string
+	Address string `yaml:"address" env-required:"true"`
 }
 
 //env-default:"production"
