eba78da30872dfe3d8f64f0a3c50dddaca95e076
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-15 22:42:38 +0530

    create new student API

--- pkg/http/handlers/student/student.go
+++ pkg/http/handlers/student/student.go
@@ -1,10 +1,30 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/kushvardhan/Students-Api/pkg/utils/response"
+	"github.com/kushvardhan/Students-Api/types"
+)
 
 func New() http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to student api"));
+
+		var student types.Student
+
+		json.NewDecoder(r.Body).Decode(&student);
+
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w,http.StatusBadRequest, err.Error())
+			return
+		} 
+		slog.Info("Creating a student");
+
+		response.WriteJson(w,http.StatusCreated, map[string]string{"success":"OK"})
 
 	}
 }
