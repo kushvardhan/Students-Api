7aebaaf28b13e47e532b7d487c098d3389735633
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-16 18:13:41 +0530

    corrected Error Handling

--- pkg/http/handlers/student/student.go
+++ pkg/http/handlers/student/student.go
@@ -11,20 +11,25 @@ import (
 	"github.com/kushvardhan/Students-Api/types"
 )
 
-func New() http.HandlerFunc{
+func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		var student types.Student
 
-		json.NewDecoder(r.Body).Decode(&student);
-
-		if errors.Is(err, io.EOF) {
-			response.WriteJson(w,http.StatusBadRequest, err.Error())
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				response.WriteJson(w, http.StatusBadRequest, "request body is empty")
+				return
+			}
+			response.WriteJson(w, http.StatusBadRequest, err.Error())
 			return
-		} 
-		slog.Info("Creating a student");
+		}
 
-		response.WriteJson(w,http.StatusCreated, map[string]string{"success":"OK"})
+		slog.Info("Creating a student", "student", student)
 
+		response.WriteJson(w, http.StatusCreated, map[string]string{
+			"success": "OK",
+		})
 	}
 }
